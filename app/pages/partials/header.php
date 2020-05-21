b691a985e7a641b998b524e420a236e602a44168
Dominic Milburn <dmilburn@steeloctopus.com>
2020-05-22 00:10:47 +1000

    Feat: Mobile Menu
    Add mobile menu to site.

--- app/pages/partials/header.php
+++ app/pages/partials/header.php
@@ -23,12 +23,14 @@
 <body>
 <div class="uk-section uk-padding-remove">
   <div class="hero uk-container">
+    <nav class="desktop-menu">
     <div class="uk-margin-small uk-padding-small uk-padding-remove-horizontal" uk-margin>
       <!--[if IE]>
       <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a
         href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
       <![endif]-->
       <a href="/" class="uk-align-left uk-margin-remove-bottom"><img src="/img/carbon_bins_logo.png" width="300"/></a>
+
       <div class="uk-align-right uk-margin-small-bottom">
       <ul class="uk-subnav uk-subnav-divider uk-flex-right" uk-margin>
         <li>
@@ -53,7 +55,7 @@
         </li>
       </ul>
       </div>
-      <nav class="uk-navbar-container uk-align-left uk-margin-remove " uk-navbar>
+      <div class="uk-navbar-container uk-align-left uk-margin-remove" uk-navbar>
         <div>
 
           <ul class="uk-navbar-nav">
@@ -101,6 +103,96 @@
             </li>
           </ul>
         </div>
-      </nav>
+      </div>
+    </div>
+    </nav>
+    <nav class="mobile-menu">
+      <div class="uk-margin-small">
+        <div class="uk-margin-remove uk-align-left">
+        <a href="/" class="uk-margin-remove-bottom">
+          <img src="/img/carbon_bins_logo.png" width="175px"/>
+        </a>
+        </div>
+        <div class="uk-margin-remove uk-align-right">
+        <a class="uk-navbar-toggle" href="#" uk-toggle="target: #offcanvas-nav">
+          <span uk-navbar-toggle-icon></span> <span class="uk-margin-small-left">Menu</span>
+        </a>
+        </div>
+      </div>
+    </nav>
+
+    <div id="offcanvas-nav" uk-offcanvas="overlay: true">
+      <div class="uk-offcanvas-bar">
+        <ul class="uk-nav uk-nav-default">
+          <li class="uk-active">
+            <a href="/pages/about-us.php">
+              <div>
+                About Us
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="/pages/how-it-works.php">
+              <div>
+                How CarbonBins Work
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="/pages/co2-removal.php">
+              <div>
+                CO<sup>2</sup> Removal
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="/pages/get-involved.php">
+              <div>
+                Get Involved
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="/pages/remove-co2.php">
+              <div>
+                Remove CO<sup>2</sup>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="/pages/faq.php">
+              <div>
+                FAQ's
+              </div>
+            </a>
+          </li>
+        </ul>
+        <p></p>
+        <ul class="uk-nav uk-nav-default">
+          <li>
+            <a href="#">
+              Contact Us
+            </a>
+          </li>
+          <li>
+            <a href="/pages/partner-with-us.php">
+              Partner with us
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              News
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Emissions Calculator
+            </a>
+          </li>
+        </ul>
+
+      </div>
     </div>
   </div>
+
+
